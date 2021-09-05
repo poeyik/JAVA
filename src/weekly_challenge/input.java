97a346581e8d34cbff1374187c9118d853fa207b
poeyik <crd9709@hansung.ac.kr>
2021-09-05 19:40:14 +0900

    Initial commit

---
+++ src/weekly_challenge/input.java
@@ -0,0 +1,15 @@
+package weekly_challenge;
+
+public class input {
+    public static void main(String[] args) {
+        long answer;
+
+        //week1 input
+        week1 s = new week1();
+        answer=s.solution(3,20,4);
+
+
+        //답 출력
+        System.out.println(answer);
+    }
+}
