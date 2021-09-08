2ed74a50d239158edbd379d5d74ebfad40ab60b9
poeyik <crd9709@hansung.ac.kr>
2021-09-08 20:34:57 +0900

    21.09.08

--- src/weekly_challenge/input.java
+++ src/weekly_challenge/input.java
@@ -2,14 +2,23 @@ package weekly_challenge;
 
 public class input {
     public static void main(String[] args) {
-        long answer;
+        int[][] scores={{100,90,98,88,65},{50,45,99,85,77},{47,88,95,80,67},{61,57,100,80,65},{24,90,94,75,65}};
+        long answer1;
+        String answer2;
 
         //week1 input
-        week1 s = new week1();
-        answer=s.solution(3,20,4);
+        week1 a1 = new week1();
+        answer1=a1.solution(3,20,4);
 
+        //week2 input
+        week2 a2 =new week2();
+        answer2=a2.solution(scores);
 
         //답 출력
-        System.out.println(answer);
+        System.out.println(answer1);
+        System.out.println(answer2);
+        System.out.println(answer1);
+        System.out.println(answer1);
+        System.out.println(answer1);
     }
 }
